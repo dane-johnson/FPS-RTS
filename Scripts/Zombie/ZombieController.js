2ce8dfec71549a438bb1c833553935bc01bfbed5
Dane Johnson <dane_johnson@ymail.com>
2016-05-27 23:58:09 -0500

    initial commit

---
+++ Scripts/Zombie/ZombieController.js
@@ -0,0 +1,40 @@
+﻿#pragma strict
+
+public var foodParent : GameObject;
+
+private var target : Transform;
+private var isFacingTarget = false;
+
+function Start(){
+  target = FindClosestChild();
+}
+
+function FixedUpdate(){
+  var rb = GetComponent(Rigidbody);
+  
+  if(!isFacingTarget){
+    turnToFaceTarget();
+  } else {
+    moveTowardsTarget();
+  }
+}
+
+function FindClosestChild(){
+  var myPos = transform.position;
+  
+  var children = foodParent.GetComponentInChildren.<Transform>();
+  
+  var closest : Transform;
+  var dist : float;
+  for(var t : Transform in children){
+    if(!closest || Vector3.Distance(myPos, t.position) < dist){
+      closest = t;
+      dist = Vector3.Distance(myPos, t.position);
+    }
+  }
+  
+  return closest;
+}
+
+function turnToFaceTarget(){}
+function moveTowardsTarget(){}
