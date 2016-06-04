3349b695a29e222bc135f7c2f3ca2f3eeb18f0c0
Dane Johnson <dane_johnson@ymail.com>
2016-06-04 17:25:27 -0500

    Added fps character

---
+++ Scripts/FPSPlayer/FPSCharacterController.js
@@ -0,0 +1,44 @@
+﻿#pragma strict
+
+var movementSpeed : float;
+var sensitivity : float;
+
+private var me : CharacterController;
+private var cameraTransform : Transform;
+
+function Start(){
+  me = GetComponent(CharacterController);
+  cameraTransform = GetComponentsInChildren(Transform)[1];
+}
+
+function FixedUpdate(){
+  
+  //Movement
+  var movement = Vector3.zero;
+  
+  movement += Vector3.forward * Input.GetAxis("Vertical");
+  movement += Vector3.right   * Input.GetAxis("Horizontal");
+  
+  movement = transform.TransformDirection(movement);
+  
+  me.Move(movement * movementSpeed * Time.deltaTime);
+  
+  //Rotation
+  
+  var rotation = Vector3.zero;
+  
+  rotation += Vector3.up * Input.GetAxis("FPSCameraHorizontal");
+  transform.Rotate(rotation * sensitivity *  Time.deltaTime);
+  
+  var cameraRotation = Vector3.right * Input.GetAxis("FPSCameraVertical");
+  
+  var currRotation = cameraTransform.localEulerAngles + cameraRotation;
+  
+  if (currRotation.x > 90 && currRotation.x < 180){
+    currRotation.x = 90;
+  } else if (currRotation.x > 180 && currRotation.x < 270){
+    currRotation.x = 270;
+  }
+  
+  cameraTransform.localEulerAngles = currRotation;
+}
---
+++ Scripts/Gamestate.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var playerClass : PlayerClass;
+
+var rtsCamera : GameObject;
+var fpsCamera : GameObject;
+
+enum PlayerClass {
+  FPS,
+  RTS
+}
+
+function Start(){
+  //cache the cameras
+  rtsCamera = GameObject.Find("RTSCamera");
+  fpsCamera = GameObject.Find("FPSCamera");
+  
+  switch (playerClass){
+  case PlayerClass.FPS:
+    rtsCamera.SetActive(false);
+    break;
+  case PlayerClass.RTS:
+    fpsCamera.SetActive(false);
+    break;
+  }
+}
